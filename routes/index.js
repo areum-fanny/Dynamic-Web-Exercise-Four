691be0f291f459517b297c94bc1022e5ef9b6344
Shakthi <sgg329@nyu.edu>
2020-11-19 12:15:59 -0500

    Express installed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const express = require("express");
 
-const app = express();
+const router = express.Router();
 
 router.get("/",(req,res) =>res.send("Exercise Four"));
 
