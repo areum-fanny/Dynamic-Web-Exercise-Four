3a8da0c72c659fe2b43b9d6dc16ecb33a36a1db3
Shakthi <sgg329@nyu.edu>
2020-11-19 13:29:30 -0500

    change

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const express = require("express");
 const app = express();
 
 const port = process.env.P0RT || 4000;
-
+var firebaseConfig = {
+    apiKey: "AIzaSyCmeBcqzdkLpVkI3UX8cDp0vxjNstvNVek",
+    authDomain: "exercise-4-8d6f0.firebaseapp.com",
+    databaseURL: "https://exercise-4-8d6f0.firebaseio.com",
+    projectId: "exercise-4-8d6f0",
+    storageBucket: "exercise-4-8d6f0.appspot.com",
+    messagingSenderId: "878998105788",
+    appId: "1:878998105788:web:ae0a4dadf8fb542c2c6ab9"
+  };
 const indexRoute = require("./routes/index.js");
 
 app.use('/',indexRoute);
