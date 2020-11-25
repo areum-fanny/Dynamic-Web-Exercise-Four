5dda896bbcc03fed403925a86e8c749ff681bfd6
Shakthi <sgg329@nyu.edu>
2020-11-24 21:56:58 -0500

    Port changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.P0RT || 3000;
+const port = process.env.PORT || 3000;
 var firebaseConfig = {
   apiKey: "AIzaSyCmeBcqzdkLpVkI3UX8cDp0vxjNstvNVek",
   authDomain: "exercise-4-8d6f0.firebaseapp.com",
